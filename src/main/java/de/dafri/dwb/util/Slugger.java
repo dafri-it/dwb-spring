fb0c39e8708e2ba8edca3fa30714bc76b33dad80
David Fritsche <daff88@gmail.com>
2025-12-23 22:22:54 -0600

    improve performance of slugger

--- src/main/java/de/dafri/dwb/util/Slugger.java
+++ src/main/java/de/dafri/dwb/util/Slugger.java
@@ -3,22 +3,17 @@ package de.dafri.dwb.util;
 public class Slugger {
 
     public static String slug(String text) {
-        String replaced = text
-                .replaceAll("ö", "oe")
-                .replaceAll("ä", "ae")
-                .replaceAll("ü", "ue")
-                .replaceAll("Ö", "Oe")
-                .replaceAll("Ä", "Ae")
-                .replaceAll("Ü", "Ue")
-                .replaceAll("ß", "ss")
-                .replaceAll("[^a-zA-Z0-9]", " ").toLowerCase()
-                ;
-
-        replaced = replaced.trim();
-        replaced = replaced.replaceAll(" {2}", " ").replaceAll(" {2}", " ").replaceAll(" {2}", " ");
-        replaced = replaced.replaceAll(" ", "-");
-
-        return replaced;
+        return text
+                .replace("ö", "oe")
+                .replace("ä", "ae")
+                .replace("ü", "ue")
+                .replace("Ö", "Oe")
+                .replace("Ä", "Ae")
+                .replace("Ü", "Ue")
+                .replace("ß", "ss")
+                .replaceAll("[^a-zA-Z0-9]+", "-")
+                .toLowerCase()
+                .trim();
     }
 
 }
